11126b96fdd2ce530cef569be2b01c2f0c4967cb
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-05 17:50:22 +0300

    changed config.py

--- app/config.py
+++ app/config.py
@@ -34,7 +34,7 @@ class Config(BaseSettings):
     POSTGRES_HOST: str
 
     model_config = SettingsConfigDict(
-        env_file='/Users/alexeyfilichkin/MainDev/WARRANTY_SUPPORT_BOT/.env'
+        env_file='../.env'
         )
 
 
