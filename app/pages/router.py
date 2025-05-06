44fdcb94c09b89788b519a46357bb14c13ecc220
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-06 11:27:54 +0300

    added registration_page to pages router

--- app/pages/router.py
+++ app/pages/router.py
@@ -2,12 +2,30 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
+from app.users.dao import UsersDAO
 from app.users.dependencies import get_optional_current_user
 
 
 router = APIRouter(tags=['Pages'])
 
 templates = Jinja2Templates(directory='app/templates')
+
+
+@router.get('/register/', response_class=HTMLResponse)
+async def registration_page(
+    request: Request
+):
+    user = await UsersDAO.find_one_or_none(email=request.get('email'))
+    if user:
+        return templates.TemplateResponse(
+            'login.html',
+            {'request': request}
+            )
+    else:
+        return templates.TemplateResponse(
+            'registration.html',
+            {'request': request}
+            )
 
 
 @router.get('/auth/login/', response_class=HTMLResponse)
